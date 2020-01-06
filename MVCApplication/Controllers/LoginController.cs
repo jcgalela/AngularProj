2edafb195499154297bff64643e2ca1c6ab6ecaa
ianpblc <publicoian@gmail.com>
2020-01-06 16:20:20 +0800

    Log in

---
+++ MVCApplication/Controllers/LoginController.cs
@@ -0,0 +1,49 @@
+﻿using MVCApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCApplication.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(User login)
+        {
+            if (ModelState.IsValid)
+            {
+                EISEntities db = new EISEntities();
+                var user = (from userlist in db.Employee
+                            where userlist.userName == login.UserName && userlist.password == login.Password
+                            select new
+                            {
+
+                                userlist.userName
+                            }).ToList();
+                if (user.FirstOrDefault() != null)
+                {
+                    Session["userName"] = user.FirstOrDefault().userName;
+
+                    return Redirect("Signup");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Invalid login credentials.");
+                }
+            }
+            return View(login);
+        }
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+    }
+}
--- MVCApplication/Models/User.cs
+++ MVCApplication/Models/User.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,24 +9,31 @@ namespace MVCApplication.Models
 {
     public class User
     {
-        public string employeeId { get; set; }
-        public string firstName { get; set; }
-        public string middleName { get; set; }
-        public string lastName { get; set; }
-        public string birthday { get; set; }
-        public string emergencyName { get; set; }
-        public string emergencyAddress { get; set; }
-        public string emergencyContact { get; set; }
-        public string employeeStatus { get; set; }
-        public string sssnum { get; set; }
-        public string tinnum { get; set; }
-        public string dateEmployed { get; set; }
-        public string dateFrom { get; set; }
-        public string dateTo { get; set; }
-        public string signature { get; set; }
-        public string userName { get; set; }
-        public string email { get; set; }
-        public string password { get; set; }
-        public string image { get; set; }
+        [Required(ErrorMessage = "Please enter user name.")]
+        [DataType(DataType.Text)]
+        [Display(Name = "User Name")]
+        [StringLength(30)]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Please enter password.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        [StringLength(10)]
+        public string Password { get; set; }
+
+      
+
+
+        [Table("Employee")]
+        public class Employee
+        {
+            [Key]
+            [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+            public string UserName { get; set; }
+            public string Password { get; set; }
+
+
+        }
     }
+
 }

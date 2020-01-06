b602dd84555332a54bc9bc73a9c745df5d58c0ba
JohnCarloTura <johncarlotura@gmail.com>
2020-01-06 14:55:09 +0800

    File created

---
+++ MVCApplication/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            ViewBag.Message = "Your Login page.";
+
+            return View();
+        }
+        public ActionResult Logout()
+        {
+
+            return View();
+        }
+        public ActionResult Signup()
+        {
+            ViewBag.Message = "Signup page.";
+
+            return View();
+        }
+        public ActionResult Changepass()
+        {
+            ViewBag.Message = "Change Password page.";
+
+            return View();
+        }
+    }
+}
